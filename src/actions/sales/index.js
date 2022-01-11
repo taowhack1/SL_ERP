ae1e8d172c92afc06e57f520055a03b681524070
pet1158888 <phaichayon.2011@gmail.com>
2022-01-11 14:57:12 +0700

    commit edit

--- src/actions/sales/index.js
+++ src/actions/sales/index.js
@@ -360,21 +360,30 @@ export const qn_actions = (data, qn_id) => (dispatch) => {
 
 export const so_actions = (data, so_id) => (dispatch) => {
   console.log("so_actions", data, so_id);
-  const Lineurl =
-    "https://1c4e-112-121-130-63.ngrok.io/api/line/post/message/push_message/so_approve";
+  // const Lineurl =
+  //   "https://1c4e-112-121-130-63.ngrok.io/api/line/post/message/push_message/so_approve";
   if (data.process_status_id >= 2 && data.process_status_id != 6) {
     console.log("sending Line :>> ", data);
-
     data.commit = 1;
-    axios.post(`${Lineurl}`, { so_id, ...data }, header_config).then((res) => {
-      console.log(res);
-      //dispatch(get_so_by_id(so_id, data.user_name));
-    });
+    axios
+      .post(
+        `${process.env.REACT_APP_LOCAL_LINE_APPROVE_SO}`,
+        { so_id, ...data },
+        header_config
+      )
+      .then((res) => {
+        console.log(res);
+        //dispatch(get_so_by_id(so_id, data.user_name));
+      });
   } else if (data.process_status_id == 6 && data.node_stay == 3) {
     console.log("sending Line :>> ", data);
     data.commit = 1;
     axios
-      .post(`${Lineurl}`, { so_id, ...data, reject: true }, header_config)
+      .post(
+        `${process.env.REACT_APP_LOCAL_LINE_APPROVE_SO}`,
+        { so_id, ...data, reject: true },
+        header_config
+      )
       .then((res) => {
         console.log(res);
         //dispatch(get_so_by_id(so_id, data.user_name));
